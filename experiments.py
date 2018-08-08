40743e365047e76933a75594b200d236b0a0add4
Sidharth Shanker <sshanker220@gmail.com>
2018-08-07 22:05:58 -0400

    Updated experiments.py to work.

--- experiments.py
+++ experiments.py
@@ -14,64 +14,6 @@ from os import mkdir
 from os.path import join, exists
 import json
 
-# Hyperparameters
-
-Actions = {
-    'INCREASE_QUADRATIC': 0,
-    'DECREASE_PERCENT': 1,
-    'INCREASE_ABSOLUTE': 2,
-    'DECREASE_ABSOLUTE': 3,
-    'STAY': 4,
-    'DECREASE_DRAMATIC': 5,
-    'UPDATE_WMAX': 6,
-    'RESET_CONGESTION_AVOIDANCE_TIME': 7
-}
-
-Rewards = {
-    'DROPPED_PACKET': -400,
-    'RTT_IS_WAY_TOO_BIG': -1000,
-    'DRAMATIC_RTT_INCREASE': -400,
-    'INCREASED_RTT': -200,
-    'MINOR_RTT_INCREASE': -100,
-    'INCREASED_CWND_ABSOLUTE': 10,
-    'INCREASED_CWND_PERCENTAGE': 15,
-    'NO_REWARD': 0
-}
-
-FEATURES = ['rtt', 'dropped_packet']
-
-HYPERPARAMETERS = {
-    'Actions': Actions,
-    'Rewards': Rewards,
-    'DRAMATIC_PERCENT_CHANGE': 0.5,
-    'ABSOLUTE_CHANGE': 5,
-    'PERCENT_CHANGE': 0.05,
-    'FEATURES': FEATURES,
-    'RTT_CHANGE_THRESHOLD': 2,
-    'RTT_DRAMATIC_CHANGE_THRESHOLD': 4,
-    'BATCH_SIZE': 30,
-    'REWARD_DECAY': 30,
-    'STATE_WINDOW_SIZE': 10,
-    'EPS_START': 0.9,
-    'EPS_END': 0.05,
-    'EPS_DECAY': 20,
-    # These constants govern training
-    'NUM_EPISODES': 2,
-    'TARGET_UPDATE': 15
-}
-
-lstm_config = {
-    "n_layers": 1,
-    "hidden_dim": 15,
-    "rdropout": .5, 
-    "input_dim": len(FEATURES),
-    "output_dim": len(Actions),
-    "bidirectional":False,
-    "num_layers": 1
-}
-
-
-
 # directory name, filename
 OUTPUT_DIRECTORY = "experiments"
 EXPERIMENT_PREFIX = "experiment_"
@@ -86,11 +28,6 @@ mahimahi_settings = {
     }
 }
 
-with open(join(OUTPUT_DIRECTORY, HYPERPARAMS_FILENAME), 'w') as hyperparams_file:
-    hyperparams_file.write( json.dumps(HYPERPARAMETERS) + "\n" )
-    hyperparams_file.write( json.dumps(lstm_config) + "\n" )
-
-
 def run_experiment(hyperparameters_file_name, experiment_name):
     experiment_dir = join(OUTPUT_DIRECTORY, EXPERIMENT_PREFIX + experiment_name)
 
@@ -99,7 +36,7 @@ def run_experiment(hyperparameters_file_name, experiment_name):
 
     if not exists(experiment_dir):
         mkdir(experiment_dir)
-    
+
     hyperparameters = None
     with open(hyperparameters_file_name) as hyperparams_file:
         hyperparameters = json.loads(hyperparams_file.read())
@@ -126,16 +63,15 @@ def run_experiment(hyperparameters_file_name, experiment_name):
         )
         print("***Episode # %d***" % i)
         run_with_mahi_settings(
-            mahimahi_settings, 
-            10, 
-            [Sender(port, strategy)], 
-            i % 1 == 0, 
-            i, 
-            write_to_disk=True, 
-            output_dir=OUTPUT_DIRECTORY, 
+            mahimahi_settings,
+            10,
+            [Sender(port, strategy)],
+            i % 1 == 0,
+            i,
+            write_to_disk=True,
+            output_dir=OUTPUT_DIRECTORY,
             experiment_prefix=experiment_dir
         )
-        
         total_losses.append(strategy.losses)
         if i % TARGET_UPDATE == 0:
             target_net.load_state_dict(policy_net.state_dict())
