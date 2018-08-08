273c6acbf84aaf40d16ce589c9b57db666727fb4
Sidharth Shanker <sshanker220@gmail.com>
2018-08-08 18:12:10 -0400

    Update experiments to run for 5 hours.

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #echo "Starting experiment 1"
 #python3 experiments.py --hyperparameters-file hyperparameters_1.json --experiment-name 1
-echo "Starting experiment 2"
-python3 experiments.py --hyperparameters-file hyperparameters_2.json --experiment-name 2
-echo "Starting experiment 3"
-python3 experiments.py --hyperparameters-file hyperparameters_3.json --experiment-name 3
-echo "Starting experiment 4"
-python3 experiments.py --hyperparameters-file hyperparameters_4.json --experiment-name 4
+echo "Starting experiment 5"
+python3 experiments.py --hyperparameters-file hyperparameters_5.json --experiment-name 5
+
+echo "Starting experiment 6"
+python3 experiments.py --hyperparameters-file hyperparameters_6.json --experiment-name 6
+
+echo "Starting experiment 7"
+python3 experiments.py --hyperparameters-file hyperparameters_7.json --experiment-name 7
+
+echo "Starting experiment 8"
+python3 experiments.py --hyperparameters-file hyperparameters_8.json --experiment-name 8
