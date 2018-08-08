b56fc7619e137b497f202fe403294a85b500b8c2
Sidharth Shanker <sshanker220@gmail.com>
2018-08-07 23:00:52 -0400

    Updated script.

---
+++ run_experiments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Starting experiment 1"
+python3 experiments.py --hyperparameters-file hyperparameters_1.json --experiment-name experiment_1
+echo "Starting experiment 2"
+python3 experiments.py --hyperparameters-file hyperparameters_2.json --experiment-name experiment_2
+echo "Starting experiment 3"
+python3 experiments.py --hyperparameters-file hyperparameters_3.json --experiment-name experiment_3
+echo "Starting experiment 4"
+python3 experiments.py --hyperparameters-file hyperparameters_4.json --experiment-name experiment_4
